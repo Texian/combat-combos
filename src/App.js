224697a610e88a5ff0e08a13aba32a0b2b1685ba
cwalters <xian04@yahoo.com>
2020-10-19 18:17:40 -0700

    Set up numerical input grid for Attributes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,44 @@ function App() {
       <div className="combo-sheet">
         <div class="attributes">
           <header>ATTRIBUTES</header>
-          <div className="attribute"></div>
-          <div className="attribute"></div>
-          <div className="attribute"></div>
-          <div className="attribute"></div>
-          <div className="attribute"></div>
-          <div className="attribute"></div>
+          <div className="grid">
+            <div className="attribute">
+              <label htmlFor="strength">Strength</label>
+              <input type="number" id="strength" name="strength" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="charisma">Charisma</label>
+              <input type="number" id="charisma" name="charisma" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="perception">Perception</label>
+              <input type="number" id="perception" name="perception" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="dexterity">Dexterity</label>
+              <input type="number" id="dexterity" name="dexterity" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="manipulation">Manipulation</label>
+              <input type="number" id="manipulation" name="manipulation" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="appearance">Appearance</label>
+              <input type="number" id="appearance" name="appearance" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="stamina">Stamina</label>
+              <input type="number" id="stamina" name="stamina" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="intelligence">Intelligence</label>
+              <input type="number" id="intelligence" name="intelligence" required min="1" max="10" size="10"/>
+            </div>
+            <div className="attribute">
+              <label htmlFor="wits">Wits</label>
+              <input type="number" id="wits" name="wits" required min="1" max="10" size="10"/>
+            </div>
+          </div>
         </div>
         <div class="combos">
           <header>COMBO</header>
