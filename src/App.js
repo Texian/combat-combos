7dffc706deac6b1b5aca6ff952f7e24d226f6201
cwalters <xian04@yahoo.com>
2020-10-19 17:02:42 -0700

    Greatly simplified CSS, slightly cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ function App() {
     <div className="App">
       <div className="combo-sheet">
         <div class="attributes">
-          <header className="header">ATTRIBUTES</header>
+          <header>ATTRIBUTES</header>
+          <div className="attribute"></div>
+          <div className="attribute"></div>
+          <div className="attribute"></div>
+          <div className="attribute"></div>
+          <div className="attribute"></div>
+          <div className="attribute"></div>
         </div>
-        <div className="moves">
-          <div class="combos">
-            <header className="header">COMBO</header>
-            <div className="combo-list"></div>
-          </div>
-          <div class="list">
-            <header className="header">MOVE LIST</header>
-            <div className="combo-list"></div>
-          </div>
+        <div class="combos">
+          <header>COMBO</header>
+          <div className="combo-list"></div>
+        </div>
+        <div class="list">
+          <header>MOVE LIST</header>
+          <div className="combo-list"></div>
         </div>
         <div class="results">
-          <header className="header">RESULTS</header>
+          <header>RESULTS</header>
         </div>
       </div>
     </div>
